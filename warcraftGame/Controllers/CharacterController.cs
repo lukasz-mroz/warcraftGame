28552c4fb9a0e1b4d63bc849e4e69934ce4f1957
Lukasz Mroz <mrozikpl@gmail.com>
2020-11-17 19:59:35 +0100

    Adding mock character for testing purpose

--- warcraftGame/Controllers/CharacterController.cs
+++ warcraftGame/Controllers/CharacterController.cs
@@ -3,16 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using warcraftGame.Models;
 
 namespace warcraftGame.Controllers
 {
   [ApiController]
-  [Route("[controller")]
+  [Route("[controller]")]
   public class CharacterController : Controller
   {
+    private static Character paladin = new Character();
     public IActionResult Index()
     {
-      return View();
+      return Ok(paladin);
     }
   }
 }
